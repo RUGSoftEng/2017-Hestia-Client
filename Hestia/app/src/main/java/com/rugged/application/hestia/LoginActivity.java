c257ec7485624448d86a912f8b03624306df075f
feikoritsema <feiko.ritsema@hotmail.com>
2017-03-21 14:43:13 +0100

    Login and remember me working. Auto redirect if remembered.

--- Hestia/app/src/main/java/com/rugged/application/hestia/LoginActivity.java
+++ Hestia/app/src/main/java/com/rugged/application/hestia/LoginActivity.java
@@ -4,17 +4,18 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import java.util.Locale;
 
-
+/* This class handles the login activity.
+ * It takes the fields from the layout, gets the values the user inputs and validates it.
+ * Furthermore it first checks the shared preferences of the phone if the user is remembered.
+ */
 public class LoginActivity extends Activity  {
     private Button loginButton;
     private EditText userField,passField;
@@ -32,68 +33,76 @@ public class LoginActivity extends Activity  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        /* Before going on, check if the user is remembered, if so, directly redirect. */
+        loginPreferences = getSharedPreferences(LOGIN_PREFERENCES, MODE_PRIVATE);
+        loginPrefsEditor = loginPreferences.edit();
+        saveLogin = loginPreferences.getBoolean("saveLogin", false);
+        if (saveLogin) {
+            gotoMainActivity();
+        }
+
+        /* Get all the buttons and fields from the layout */
         loginButton = (Button)findViewById(R.id.loginButton);
         userField = (EditText)findViewById(R.id.username);
         passField = (EditText)findViewById(R.id.password);
         rememberButton = (CheckBox) findViewById(R.id.rememberButton);
-
         attemptsText = (TextView)findViewById(R.id.textView4);
         attemptsText.setVisibility(View.GONE);
 
-
-        loginPreferences = getSharedPreferences(LOGIN_PREFERENCES, MODE_PRIVATE);
-        loginPrefsEditor = loginPreferences.edit();
-
-        saveLogin = loginPreferences.getBoolean("saveLogin", false);
-        if (saveLogin) {
-            userField.setText(loginPreferences.getString("username", ""));
-            passField.setText(loginPreferences.getString("password", ""));
-            rememberButton.setChecked(true);
-        }
-
         loginButton.setOnClickListener(new View.OnClickListener() {
+            /* Button has been clicked, get and check credentials */
             @Override
             public void onClick(View v) {
                 username = userField.getText().toString();
                 password = passField.getText().toString();
                 if(checkCredentials(username,password)){
                     if (rememberButton.isChecked()) {
-                        loginPrefsEditor.putBoolean("saveLogin", true);
-                        loginPrefsEditor.putString("username", username);
-                        loginPrefsEditor.putString("password", password);
-                        loginPrefsEditor.apply();
+                        setSaveLogin(username,password);
                     } else {
-                        loginPrefsEditor.clear();
-                        loginPrefsEditor.apply();
+                        clearSaveLogin();
                     }
-                    // TODO: Redirect to the PeripheralListActivity
-                    Toast.makeText(getApplicationContext(),
-                            "Correct, redirecting now.",Toast.LENGTH_SHORT).show();
-                    gotoPeripheralListActivity();
-
+                    showLoginToast("Correct, redirecting...");
+                    gotoMainActivity();
                 }else{
-                    Toast.makeText(getApplicationContext(),
-                            "Wrong Credentials",Toast.LENGTH_SHORT).show();
-
-                    attemptsText.setVisibility(View.VISIBLE);
-                    counter--;
-                    attemptsText.setText(String.format(Locale.getDefault(), "%d",counter));
-
-                    if (counter == 0) {
-                        loginButton.setEnabled(false);
-                    }
+                    showLoginToast("Wrong credentials.");
+                    editLoginAttempts();
                 }
             }
         });
     }
 
-    public boolean checkCredentials(String username,String password){
+    private boolean checkCredentials(String username,String password){
         // TODO: Check credentials with server database
-        return(username.equals("admin")&&password.equals("admin"));
+        return(username.equals("admin")&&password.equals("password"));
     }
 
-    public void gotoPeripheralListActivity(){
+    private void gotoMainActivity(){
         Intent i = new Intent(LoginActivity.this, PeripheralListActivity.class);
         startActivity(i);
+    }
+
+    private void setSaveLogin(String username, String password){
+        loginPrefsEditor.putBoolean("saveLogin", true);
+        loginPrefsEditor.putString("username", username);
+        loginPrefsEditor.putString("password", password);
+        loginPrefsEditor.apply();
+    }
+
+    private void clearSaveLogin(){
+        loginPrefsEditor.clear();
+        loginPrefsEditor.apply();
+    }
+
+    private void editLoginAttempts(){
+        attemptsText.setVisibility(View.VISIBLE);
+        counter--;
+        attemptsText.setText(String.format(Locale.getDefault(), "%d",counter));
+        if (counter == 0) {
+            loginButton.setEnabled(false);
+        }
+    }
+
+    private void showLoginToast(String info){
+        Toast.makeText(getApplicationContext(),info,Toast.LENGTH_SHORT).show();
     }
 }
