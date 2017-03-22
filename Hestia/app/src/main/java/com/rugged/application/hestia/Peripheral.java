59c3a0bc23477cf4a9379e6b830e2f3c847e4961
markhelmus <m.a.helmus@student.rug.nl>
2017-03-22 16:25:12 +0100

    Changed Peripheral to the old class (will be updated in another branch)

--- Hestia/app/src/main/java/com/rugged/application/hestia/Peripheral.java
+++ Hestia/app/src/main/java/com/rugged/application/hestia/Peripheral.java
@@ -7,24 +7,15 @@ import java.util.ArrayList;
  * server.
  */
 public class Peripheral {
-    private int deviceId;
-    private String name;
     private String type;
-    ArrayList<Activator> activators;
-
-    public Device(int id, String name, String type, ArrayList<Activator> a) {
-        this.deviceId = id;
-        this.name = name;
-        this.type = type;
-        this.activators = a;
-    }
+    private int id;
 
     public int getId() {
-        return deviceId;
+        return id;
     }
 
     public void setId(int id) {
-        this.deviceId = id;
+        this.id = id;
     }
 
     public String getType() {
@@ -34,21 +25,4 @@ public class Peripheral {
     public void setType(String type) {
         this.type = type;
     }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public ArrayList<Activator> getActivators() {
-        return activators;
-    }
-
-    public void setActivators(ArrayList<Activator> activators) {
-        this.activators = activators;
-    }
-
 }
