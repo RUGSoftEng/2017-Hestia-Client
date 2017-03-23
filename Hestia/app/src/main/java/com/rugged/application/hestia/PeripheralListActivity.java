d50e4308174172012530ff8f5469010ef97ee100
markhelmus <m.a.helmus@student.rug.nl>
2017-03-23 15:05:15 +0100

    Added RequestHandler

--- Hestia/app/src/main/java/com/rugged/application/hestia/PeripheralListActivity.java
+++ Hestia/app/src/main/java/com/rugged/application/hestia/PeripheralListActivity.java
@@ -1,13 +1,11 @@
 package com.rugged.application.hestia;
 
 import android.support.v4.app.Fragment;
-import android.util.Log;
 
 public class PeripheralListActivity extends SingleFragmentActivity{
     private final static String TAG = "PeripheralListActivity";
     @Override
     protected Fragment createFragment() {
-        Log.i(TAG, "Getting here");
         return new PeripheralListFragment();
     }
 }
--- Hestia/app/src/main/java/com/rugged/application/hestia/PeripheralListFragment.java
+++ Hestia/app/src/main/java/com/rugged/application/hestia/PeripheralListFragment.java
@@ -1,7 +1,6 @@
 package com.rugged.application.hestia;
 import android.content.Context;
 import android.graphics.Typeface;
-import android.net.http.RequestQueue;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
