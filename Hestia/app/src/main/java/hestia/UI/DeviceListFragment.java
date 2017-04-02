fad72f9206790b2bee3a587ade051cb7719fd781
markhelmus <m.a.helmus@student.rug.nl>
2017-04-03 01:13:22 +0200

    Changed color of Floating Action Button

--- Hestia/app/src/main/java/hestia/UI/DeviceListFragment.java
+++ Hestia/app/src/main/java/hestia/UI/DeviceListFragment.java
@@ -1,4 +1,6 @@
 package hestia.UI;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
