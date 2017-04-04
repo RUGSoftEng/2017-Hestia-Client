ad957c446877b454d60de6ad33299194d81e01b7
Stefan Evanghelides <stefan.evanghelides@gmail.com>
2017-04-04 22:58:28 +0200

    Fixed #23 - DELETE request works properly
    Removed the final "/" from the path. It fixed everything. Thanks Lars
    for pointing it out.

--- Hestia/app/src/main/java/hestia/backend/RemoveDeviceTask.java
+++ Hestia/app/src/main/java/hestia/backend/RemoveDeviceTask.java
@@ -2,7 +2,10 @@ package hestia.backend;
 
 import android.os.AsyncTask;
 import android.util.Log;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -38,13 +41,32 @@ public class RemoveDeviceTask extends AsyncTask<Void, Void, Void> {
         try {
             url = new URL(path);
             httpCon = (HttpURLConnection) url.openConnection();
-            //httpCon.setDoOutput(true);
             httpCon.setRequestMethod("DELETE");
+            httpCon.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            httpCon.setRequestProperty("charset", "utf-8");
             httpCon.connect();
         } catch (IOException e) {
             Log.e(TAG, "Connection failed: could not realize the DELETE request");
         } finally {
-            httpCon.disconnect();
+            if(httpCon != null) {
+                // FOR DEBUGGING ONLY -> PRINTING THE RESPONSE CODE
+
+
+                BufferedReader br = null;
+                try {
+                    System.out.println("Response code: " + httpCon.getResponseCode());
+                    br = new BufferedReader(new InputStreamReader(httpCon.getInputStream()));
+                    String line, responseText = "";
+                    while ((line = br.readLine()) != null) {
+                        System.out.println("LINE: "+line);
+                        responseText += line;
+                    }
+                    br.close();
+                } catch (IOException e) {
+                    Log.e(TAG, "INSIDE FIRST FINALLY CLAUSE: ERROR WITH IOEXCEPTION");
+                }
+                httpCon.disconnect();
+            }
         }
         return null;
     }
@@ -55,6 +77,6 @@ public class RemoveDeviceTask extends AsyncTask<Void, Void, Void> {
      */
     private void updatePath() {
         int id = this.device.getDeviceId();
-        this.path = this.path + "devices/" + id + "/";
+        this.path = this.path + "devices/" + id;
     }
 }
