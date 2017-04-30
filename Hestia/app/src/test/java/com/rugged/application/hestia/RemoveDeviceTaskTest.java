4af996f8a211e89f5ed61076481800bee6e07a3b
Stefan Evanghelides <stefan.evanghelides@gmail.com>
2017-04-30 15:14:54 +0200

    first implementation of asyncTask test case.

--- Hestia/app/src/test/java/com/rugged/application/hestia/RemoveDeviceTaskTest.java
+++ Hestia/app/src/test/java/com/rugged/application/hestia/RemoveDeviceTaskTest.java
@@ -1,22 +1,43 @@
 package com.rugged.application.hestia;
 
+import android.test.UiThreadTest;
+
+import junit.framework.TestCase;
+
 import org.junit.Test;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
+import hestia.backend.Device;
 import hestia.backend.RemoveDeviceTask;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 import static org.junit.Assert.*;
 
-public class RemoveDeviceTaskTest {
-    @Test
-    public void test1() throws ExecutionException, InterruptedException {
-        //  create mock
-        RemoveDeviceTask test = mock(RemoveDeviceTask.class);
-        Integer response = test.execute().get();
-        assertTrue(response!=null && response == 204);
+public class RemoveDeviceTaskTest extends TestCase {
+
+    private CountDownLatch signal;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        signal = new CountDownLatch(1);
     }
+
+    public void testAsyncTask() throws InterruptedException
+    {
+        new RemoveDeviceTask(new Device(1, null, null, null));
+        signal.await(5, TimeUnit.SECONDS);
+        assertTrue( "assert something meaningful here", true );
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
 
 }
