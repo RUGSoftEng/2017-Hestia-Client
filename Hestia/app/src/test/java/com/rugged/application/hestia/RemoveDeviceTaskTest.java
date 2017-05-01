fc9887eb889fbff9fc531699435896d58284eb77
Stefan Evanghelides <stefan.evanghelides@gmail.com>
2017-05-01 19:49:17 +0200

    Finished RemoveDeviceTask

--- Hestia/app/src/test/java/com/rugged/application/hestia/RemoveDeviceTaskTest.java
+++ Hestia/app/src/test/java/com/rugged/application/hestia/RemoveDeviceTaskTest.java
@@ -1,28 +1,46 @@
 package com.rugged.application.hestia;
 
-import junit.framework.TestCase;
-
+import android.os.AsyncTask;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 import hestia.backend.RemoveDeviceTask;
+import static junit.framework.TestCase.assertTrue;
 import static org.mockito.Mockito.*;
 
-public class RemoveDeviceTaskTest extends TestCase {
+@RunWith(JUnit4.class)
+public class RemoveDeviceTaskTest {
+    final RemoveDeviceTask mockRemoveDevice = mock(RemoveDeviceTask.class);
 
     @Test
-    public void testWithMock() throws Exception {
-        final RemoveDeviceTask mockRemoveDevice = mock(RemoveDeviceTask.class);
-        when(mockRemoveDevice.doInBackground()).thenReturn(204);
-        when(mockRemoveDevice.execute()).then(new Answer<Boolean>() {
+    public void executeTest() throws Exception {
+        when(mockRemoveDevice.execute()).then(new Answer<AsyncTask>() {
             @Override
-            public Boolean answer(InvocationOnMock invocation) throws Throwable {
+            public AsyncTask answer(InvocationOnMock invocation) throws Throwable {
                 when(mockRemoveDevice.get()).thenReturn(204);
-                verify(mockRemoveDevice).get();
-                return true;
+                return mockRemoveDevice;
             }
         });
+        mockRemoveDevice.execute();
+        verify(mockRemoveDevice).execute();
+        int result = mockRemoveDevice.get();
+        verify(mockRemoveDevice).get();
+        assertTrue(result==204);
+    }
+
+    @Test
+    public void doInBackgroundTest() {
+        when(mockRemoveDevice.doInBackground()).thenReturn(204);
         mockRemoveDevice.doInBackground();
         verify(mockRemoveDevice).doInBackground();
+    }
+
+    @Test
+    public void onPostExecuteTest() {
+        doNothing().when(mockRemoveDevice).onPostExecute(anyInt());
+        mockRemoveDevice.onPostExecute(anyInt());
+        verify(mockRemoveDevice).onPostExecute(anyInt());
     }
 }
