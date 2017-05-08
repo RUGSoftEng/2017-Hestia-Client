fe893d755dfeabcf0130f90e6ddfdd4812573b6d
FrankTN <f.g.te.nijenhuis@student.rug.nl>
2017-05-09 00:10:02 +0200

    Removed unnecessary import

--- Hestia/app/src/main/java/hestia/UI/DeviceListActivity.java
+++ Hestia/app/src/main/java/hestia/UI/DeviceListActivity.java
@@ -3,7 +3,6 @@ package hestia.UI;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutCompat;
 
 import hestia.backend.BackendInteractor;
 
