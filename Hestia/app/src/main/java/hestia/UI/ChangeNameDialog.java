f2c57b5821c382cfb8108634618b56ec1c6a05cf
markhelmus <m.a.helmus@student.rug.nl>
2017-05-21 18:59:49 +0200

    Make ready for pull request (refactor/dialog into this branch)

---
+++ Hestia/app/src/main/java/hestia/UI/ChangeNameDialog.java
@@ -0,0 +1,13 @@
+package hestia.UI;
+
+import android.app.Activity;
+
+/**
+ * Created by Mark on 21-5-2017.
+ */
+
+public class ChangeNameDialog extends HestiaDialog {
+    public ChangeNameDialog(Activity activity, int reference, String title) {
+        super(activity, reference, title);
+    }
+}
