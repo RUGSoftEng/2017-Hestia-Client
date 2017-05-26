42582ea8cdd7c2f60fb969482d5fcc16e27799a1
Stefan Evanghelides <stefan.evanghelides@gmail.com>
2017-05-26 14:12:52 +0200

    small changes. Throw more ComFaultErrors

--- Hestia/app/src/main/java/hestia/UI/dialogs/AddDeviceInfo.java
+++ Hestia/app/src/main/java/hestia/UI/dialogs/AddDeviceInfo.java
@@ -15,6 +15,7 @@ import java.io.IOException;
 import java.util.HashMap;
 
 import hestia.backend.ServerCollectionsInteractor;
+import hestia.backend.exceptions.ComFaultException;
 import hestia.backend.models.RequiredInfo;
 
 /**
@@ -97,6 +98,11 @@ public class AddDeviceInfo extends HestiaDialog {
                 } catch (IOException e) {
                     Toast.makeText(context, "Something went wrong", Toast.LENGTH_SHORT).show();
                     e.printStackTrace();
+                } catch (ComFaultException e) {
+                    e.printStackTrace();
+                    String error = e.getError();
+                    String message = e.getMessage();
+                    Toast.makeText(context, error + ":" + message, Toast.LENGTH_SHORT).show();
                 }
                 return 0;
             }
