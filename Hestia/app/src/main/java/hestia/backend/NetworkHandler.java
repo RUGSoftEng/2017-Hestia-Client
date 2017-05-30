9e3b7acff01532848b17d253b5de857025ff2aec
FrankTN <f.g.te.nijenhuis@student.rug.nl>
2017-05-30 15:14:12 +0200

    Changed httpurlconnection to httpsurl in the NH

--- Hestia/app/src/main/java/hestia/backend/NetworkHandler.java
+++ Hestia/app/src/main/java/hestia/backend/NetworkHandler.java
@@ -15,6 +15,8 @@ import java.lang.reflect.Type;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import javax.net.ssl.HttpsURLConnection;
+
 /**
  * A singleton class which handles interaction between front and back-end. It contains methods
  * for sending 4 types of requests (GET, PUT, POST AND DELETE), along with additional methods
@@ -70,6 +72,26 @@ public class NetworkHandler extends Application {
         String path = this.getDefaultPath() + endpoint;
         URL url = new URL(path);
         HttpURLConnection connector = (HttpURLConnection) url.openConnection();
+        connector.setReadTimeout(2000);
+        connector.setConnectTimeout(2000);
+        connector.setRequestMethod(requestMethod);
+        connector.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+        return connector;
+    }
+
+    /**
+     * This method establishes the connection to the server, by setting the type of request
+     * and the path.
+     * @param requestMethod the type of request that will be sent to the server.
+     * @param endpoint path to the server's endpoint.
+     * @return the object responsible for setting up the connection to the server.
+     * @throws IOException
+     */
+    private HttpsURLConnection connectToSecureServer(String requestMethod, String endpoint) throws IOException {
+
+        String path = this.getDefaultPath() + endpoint;
+        URL url = new URL(path);
+        HttpsURLConnection connector = (HttpsURLConnection) url.openConnection();
         connector.setReadTimeout(2000);
         connector.setConnectTimeout(2000);
         connector.setRequestMethod(requestMethod);
@@ -157,3 +179,103 @@ public class NetworkHandler extends Application {
         return this.getIp().equals(networkHandler.getIp());
     }
 }
+
+/*
+package com.mkyong.client;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.security.cert.Certificate;
+import java.io.*;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLPeerUnverifiedException;
+
+public class HttpsClient{
+
+   public static void main(String[] args)
+   {
+        new HttpsClient().testIt();
+   }
+
+   private void testIt(){
+
+      String https_url = "https://www.google.com/";
+      URL url;
+      try {
+
+	     url = new URL(https_url);
+	     HttpsURLConnection con = (HttpsURLConnection)url.openConnection();
+
+	     //dumpl all cert info
+	     print_https_cert(con);
+
+	     //dump all the content
+	     print_content(con);
+
+      } catch (MalformedURLException e) {
+	     e.printStackTrace();
+      } catch (IOException e) {
+	     e.printStackTrace();
+      }
+
+   }
+
+   private void print_https_cert(HttpsURLConnection con){
+
+    if(con!=null){
+
+      try {
+
+	System.out.println("Response Code : " + con.getResponseCode());
+	System.out.println("Cipher Suite : " + con.getCipherSuite());
+	System.out.println("\n");
+
+	Certificate[] certs = con.getServerCertificates();
+	for(Certificate cert : certs){
+	   System.out.println("Cert Type : " + cert.getType());
+	   System.out.println("Cert Hash Code : " + cert.hashCode());
+	   System.out.println("Cert Public Key Algorithm : "
+                                    + cert.getPublicKey().getAlgorithm());
+	   System.out.println("Cert Public Key Format : "
+                                    + cert.getPublicKey().getFormat());
+	   System.out.println("\n");
+	}
+
+	} catch (SSLPeerUnverifiedException e) {
+		e.printStackTrace();
+	} catch (IOException e){
+		e.printStackTrace();
+	}
+
+     }
+
+   }
+
+   private void print_content(HttpsURLConnection con){
+	if(con!=null){
+
+	try {
+
+	   System.out.println("****** Content of the URL ********");
+	   BufferedReader br =
+		new BufferedReader(
+			new InputStreamReader(con.getInputStream()));
+
+	   String input;
+
+	   while ((input = br.readLine()) != null){
+	      System.out.println(input);
+	   }
+	   br.close();
+
+	} catch (IOException e) {
+	   e.printStackTrace();
+	}
+
+       }
+
+   }
+
+}
+ */
