78aa5002b0230f91cce6f16126759c27b90a27ef
Stefan Evanghelides <stefan.evanghelides@gmail.com>
2017-05-31 15:29:45 +0200

    added tests for ComFaultException; added setters as well

--- Hestia/app/src/androidTest/java/com/rugged/application/hestia/backend/exceptions/ComFaultExceptionTest.java
+++ Hestia/app/src/androidTest/java/com/rugged/application/hestia/backend/exceptions/ComFaultExceptionTest.java
@@ -1,4 +1,45 @@
 package com.rugged.application.hestia.backend.exceptions;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import hestia.backend.exceptions.ComFaultException;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.assertNotSame;
+
+@RunWith(JUnit4.class)
 public class ComFaultExceptionTest {
+    private String DEFAULT_ERROR;
+    private String DEFAULT_MESSAGE;
+    private ComFaultException dummyComFaultException;
+
+    @Before
+    public void setUp() {
+        DEFAULT_ERROR = "default_error";
+        DEFAULT_MESSAGE = "default_message";
+        dummyComFaultException = new ComFaultException(DEFAULT_ERROR, DEFAULT_MESSAGE);
+        assertNotNull(dummyComFaultException);
+    }
+
+    @Test
+    public void setAndGetErrorTest() {
+        assertEquals(DEFAULT_ERROR, dummyComFaultException.getError());
+        String newError = "new_error";
+        assertNotSame(DEFAULT_ERROR, newError);
+        dummyComFaultException.setError(newError);
+        assertEquals(newError, dummyComFaultException.getError());
+    }
+
+    @Test
+    public void setAndGetMessageTest() {
+        assertEquals(DEFAULT_MESSAGE, dummyComFaultException.getMessage());
+        String newMessage = "new_message";
+        assertNotSame(DEFAULT_MESSAGE, newMessage);
+        dummyComFaultException.setMessage(newMessage);
+        assertEquals(newMessage, dummyComFaultException.getMessage());
+    }
 }
