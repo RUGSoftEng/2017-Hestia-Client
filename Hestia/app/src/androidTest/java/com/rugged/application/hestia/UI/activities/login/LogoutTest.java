c83be64ea6c8e8ee0e55f4c635e24faeba334bfb
feikoritsema <feiko.ritsema@hotmail.com>
2017-06-01 14:10:23 +0200

    Fixed logoutTest, deleted loginActivityTest

--- Hestia/app/src/androidTest/java/com/rugged/application/hestia/UI/activities/login/LogoutTest.java
+++ Hestia/app/src/androidTest/java/com/rugged/application/hestia/UI/activities/login/LogoutTest.java
@@ -1,14 +1,25 @@
 package com.rugged.application.hestia.UI.activities.login;
 
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import hestia.UI.activities.home.HomeActivity;
+
+import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.intent.Intents.intended;
 import static android.support.test.espresso.intent.matcher.ComponentNameMatchers.hasShortClassName;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.toPackage;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static com.rugged.application.hestia.R.id.context_menu;
 import static org.hamcrest.Matchers.allOf;
 
-/**
 
 @RunWith(AndroidJUnit4.class)
 public class LogoutTest {
@@ -17,21 +28,19 @@ public class LogoutTest {
 
 
     @Rule
-    public IntentsTestRule<DeviceListActivity> mIntentsRule =
-            new IntentsTestRule<>(DeviceListActivity.class);
+    public IntentsTestRule<HomeActivity> mIntentsRule =
+            new IntentsTestRule<>(HomeActivity.class);
 
     @Test
     public void logoutTest(){
-        // Logout
+
         onView(withId(context_menu)).perform(click());
 
         onView(withText(LOGOUT_TEXT)).perform(click());
 
         intended(allOf(
-                hasComponent(hasShortClassName("hestia.UI.Activities.Login.LoginActivity")),
+                hasComponent(hasShortClassName("hestia.UI.activities.login.LoginActivity")),
                 toPackage(PACKAGE_NAME)));
     }
 
 }
-
- */
--- Hestia/app/src/androidTest/java/com/rugged/application/hestia/UI/dialogs/IpDialogTest.java
+++ Hestia/app/src/androidTest/java/com/rugged/application/hestia/UI/dialogs/IpDialogTest.java
@@ -1,6 +1,5 @@
 package com.rugged.application.hestia.UI.dialogs;
 
-import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
