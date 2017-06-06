156a0ccf0192e726178062907915fb33f12ed889
Lars Holdijk <l.g.holdijk@gmail.com>
2017-06-06 22:21:05 +0200

    Previous changes broke some stuff. Reverted to make it work

--- Hestia/app/src/main/java/hestia/UI/HestiaApplication.java
+++ Hestia/app/src/main/java/hestia/UI/HestiaApplication.java
@@ -3,12 +3,16 @@ package hestia.UI;
 import android.app.Application;
 import android.content.Context;
 import android.content.SharedPreferences;
+
 import com.rugged.application.hestia.R;
+
 import hestia.backend.NetworkHandler;
+
 /**
- * An extension of the Application class provided by Android, which additionally contains the
- * NetworkHandler singleton.
+ * An extension of the Application class provided by Android, which additionally creates
+ * the network handler
  */
+
 
 public class HestiaApplication extends Application {
 
@@ -27,7 +31,7 @@ public class HestiaApplication extends Application {
         SharedPreferences prefs = getSharedPreferences(HESTIA_IP, Context.MODE_PRIVATE);
         String ip = prefs.getString(SERVER_IP, getApplicationContext().getString(R.string.default_ip));
         networkHandler = new NetworkHandler(ip
-                , R.string.default_port);
+                , Integer.valueOf(getApplicationContext().getString(R.string.default_port)));
     }
 
     public NetworkHandler getNetworkHandler(){
